2d574dee03c7d8fd77616b1aec8d1f193c2d9d55
Aidar Khusainov <khusainov19978@gmail.com>
2026-06-11 13:50:54 +0500

    packaging: include Debian doc metadata

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -60,6 +60,8 @@ install -m 0644 packaging/sysusers.d/tunwarden.conf "${root_dir}/usr/lib/sysuser
 gzip -n -c docs/man/tunwarden.1 > "${root_dir}/usr/share/man/man1/tunwarden.1.gz"
 gzip -n -c docs/man/tunwardend.8 > "${root_dir}/usr/share/man/man8/tunwardend.8.gz"
 install -m 0644 README.md LICENSE "${root_dir}/usr/share/doc/tunwarden/"
+install -m 0644 LICENSE "${root_dir}/usr/share/doc/tunwarden/copyright"
+printf 'tunwarden (%s) unstable; urgency=medium\n\n  * Local development package build.\n\n -- Aidar Khusainov <19706697+AidarKhusainov@users.noreply.github.com>  Thu, 11 Jun 2026 00:00:00 +0000\n' "${version}" | gzip -n -c > "${root_dir}/usr/share/doc/tunwarden/changelog.gz"
 find docs -type f ! -path 'docs/man/*' -print | while IFS= read -r file; do
   target="${root_dir}/usr/share/doc/tunwarden/${file}"
   mkdir -p "$(dirname "${target}")"
