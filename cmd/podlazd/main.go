74868ae4c2eda04e975deb9384d0842ea9c1e16b
Aidar Khusainov <khusainov19978@gmail.com>
2026-06-18 16:36:50 +0500

    Fill podlazd daemon entrypoint

--- cmd/podlazd/main.go
+++ cmd/podlazd/main.go
@@ -1,1 +1,16 @@
-// placeholder
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/AidarKhusainov/podlaz/internal/app/daemon"
+)
+
+func main() {
+	if err := daemon.Run(context.Background(), os.Args[1:]); err != nil {
+		fmt.Fprintln(os.Stderr, "podlazd:", err)
+		os.Exit(1)
+	}
+}
