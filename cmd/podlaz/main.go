7b23162b58e6b4e4d949d8c0133b7a2f0047ee40
Aidar Khusainov <khusainov19978@gmail.com>
2026-06-18 16:35:12 +0500

    Fill podlaz CLI entrypoint

--- cmd/podlaz/main.go
+++ cmd/podlaz/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/AidarKhusainov/podlaz/internal/app/cli"
+)
+
+func main() {
+	if err := cli.Run(context.Background(), os.Args[1:]); err != nil {
+		fmt.Fprintln(os.Stderr, "podlaz:", err)
+		os.Exit(cli.ExitCode(err))
+	}
+}
