148f62b92701b943c768bf0fd95a132375414175
Aidar Khusainov <khusainov19978@gmail.com>
2026-07-07 16:41:31 +0500

    Validate packaged Xray TUN schema

--- scripts/ci/validate-packages.sh
+++ scripts/ci/validate-packages.sh
@@ -66,6 +66,21 @@ validate_executable_mode() {
   grep -E '^-rwxr-xr-x' <<<"${listing}"
 }
 
+run_native_packaged_xray_schema_test() {
+  local package="$1"
+  local arch="$2"
+  local host_arch
+  host_arch="$(dpkg --print-architecture)"
+  if [ "${arch}" != "${host_arch}" ]; then
+    echo "Skipping packaged Xray config test for ${arch} on ${host_arch} host"
+    return 0
+  fi
+  local xray="/tmp/podlaz-${arch}-packaged-xray-schema-test"
+  dpkg-deb --fsys-tarfile "${package}" | tar -xOf - ./usr/lib/podlaz/xray > "${xray}"
+  chmod 0755 "${xray}"
+  PODLAZ_PACKAGED_XRAY_PATH="${xray}" go test ./internal/daemon -run '^TestPackagedXrayAcceptsPinnedTunConfigs$' -count=1
+}
+
 for package in "$@"; do
   test -f "${package}"
 
@@ -128,6 +143,7 @@ for package in "$@"; do
   validate_binary_architecture "${package}" "${arch}" podlazd
   validate_elf_architecture "${package}" "${arch}" "/usr/lib/podlaz/xray" "xray"
   validate_executable_mode "${package}" "/usr/lib/podlaz/xray"
+  run_native_packaged_xray_schema_test "${package}" "${arch}"
 
   dpkg-deb --fsys-tarfile "${package}" | tar -xOf - ./usr/lib/systemd/system/podlazd.service > "${service}"
   dpkg-deb --fsys-tarfile "${package}" | tar -xOf - ./usr/lib/sysusers.d/podlaz.conf > "${sysusers}"
@@ -141,6 +157,7 @@ for package in "$@"; do
   grep -Fx 'Group=podlaz' "${service}"
   grep -Fx 'UMask=0077' "${service}"
   grep -Fx 'Environment=PODLAZ_XRAY_PATH=/usr/lib/podlaz/xray' "${service}"
+  assert_no_fixed_match 'PODLAZ_POLKIT_AUTHORIZATION=required' "${service}"
   assert_no_fixed_match 'PODLAZ_TUN2SOCKS_PATH' "${service}"
   grep -Fx 'RuntimeDirectory=podlaz' "${service}"
   grep -Fx 'RuntimeDirectoryMode=0711' "${service}"
@@ -171,6 +188,7 @@ if [ -n "${PODLAZ_LINKAGE_ROOT:-}" ]; then
   ldd "${PODLAZ_LINKAGE_ROOT}/usr/bin/podlaz"
   ldd "${PODLAZ_LINKAGE_ROOT}/usr/bin/podlazd"
   ldd "${PODLAZ_LINKAGE_ROOT}/usr/bin/podlaz" | grep -F 'libc.so.6'
+  ldd "${PODLAZ_LINKAGE_ROOT}/usr/bin/podlazd"
   ldd "${PODLAZ_LINKAGE_ROOT}/usr/bin/podlazd" | grep -F 'libc.so.6'
 fi
 
