c8b090bd57d19717754d51af5bf3be64b7cab46d
Aidar Khusainov <khusainov19978@gmail.com>
2026-07-21 18:00:34 +0500

    fix: prioritize lifecycle failure classifications

--- internal/tundiag/lifecycle_classification.go
+++ internal/tundiag/lifecycle_classification.go
@@ -4,3 +4,10 @@ const (
 	ClassNetworkApplyFailure  Classification = "network_apply_failure"
 	ClassNetworkVerifyFailure Classification = "network_verify_failure"
 )
+
+func init() {
+	primaryClassificationPriority = append([]Classification{
+		ClassNetworkApplyFailure,
+		ClassNetworkVerifyFailure,
+	}, primaryClassificationPriority...)
+}
