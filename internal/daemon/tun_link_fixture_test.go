20f7a0cc30bcbdb1c3ec71a31c5ef1ee35cd342d
Aidar Khusainov <khusainov19978@gmail.com>
2026-08-06 16:21:26 +0500

    test: add production oneline tun link fixture

---
+++ internal/daemon/tun_link_fixture_test.go
@@ -0,0 +1,3 @@
+package daemon
+
+const productionTunLinkOnelineForTest = "7: podlaz0: <POINTOPOINT,NOARP,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UNKNOWN mode DEFAULT group default qlen 500 link/none tun type tun pi off vnet_hdr on persist off"
