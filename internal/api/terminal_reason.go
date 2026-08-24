5de9fdd7bfe63e5add29381a46def4b9355f1dfd
Aidar Khusainov <khusainov19978@gmail.com>
2026-08-24 13:09:08 +0500

    style: format terminal reason API

--- internal/api/terminal_reason.go
+++ internal/api/terminal_reason.go
@@ -8,8 +8,8 @@ import "fmt"
 type TerminalReason string
 
 const (
-	TerminalReasonVPNConnectFailed   TerminalReason = "vpn_connect_failed"
-	TerminalReasonVPNRestoreFailed   TerminalReason = "vpn_restore_failed"
+	TerminalReasonVPNConnectFailed    TerminalReason = "vpn_connect_failed"
+	TerminalReasonVPNRestoreFailed    TerminalReason = "vpn_restore_failed"
 	TerminalReasonBootNetworkNotReady TerminalReason = "boot_network_not_ready"
 )
 
