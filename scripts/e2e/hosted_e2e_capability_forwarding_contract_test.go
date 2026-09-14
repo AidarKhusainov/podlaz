01175ee8adbd9908e807f3c2ea873b5d47c4aeea
Aidar Khusainov <khusainov19978@gmail.com>
2026-09-14 17:38:26 +0500

    test: assert persistent hosted package diagnostics

--- scripts/e2e/hosted_e2e_capability_forwarding_contract_test.go
+++ scripts/e2e/hosted_e2e_capability_forwarding_contract_test.go
@@ -24,14 +24,10 @@ func TestHostedE2ECapabilityObservesForwardDropWithoutRejectingRunner(t *testing
 }
 
 func TestHostedE2ECapabilityPreservesBoundedPackageFailureDiagnostics(t *testing.T) {
-	script := readHostedCapabilityFile(t, hostedCapabilityScript)
 	workflow := readHostedCapabilityFile(t, hostedCapabilityWorkflow)
-	requireHostedCapabilityMarkers(t, script,
-		"candidate-install.log",
-		"CAPABILITY_PRIVATE",
-	)
 	requireHostedCapabilityMarkers(t, workflow,
 		"Collect bounded package install diagnostics",
+		"system-guest/var/log/apt/term.log",
 		"hosted-capability-private/candidate-install.log",
 		"tail -n 80",
 	)
