5a259b4b600ca01e2148a878fdff09afa087a596
Aidar Khusainov <khusainov19978@gmail.com>
2026-09-14 16:34:14 +0500

    test: scope forward-policy evidence to outer runner

--- scripts/e2e/hosted_e2e_capability_forward_policy_contract_test.go
+++ scripts/e2e/hosted_e2e_capability_forward_policy_contract_test.go
@@ -3,8 +3,8 @@ package e2e_test
 import "testing"
 
 func TestHostedE2ECapabilityReportsOuterForwardPolicy(t *testing.T) {
-	script := readHostedCapabilityFile(t, hostedCapabilityScript)
-	requireHostedCapabilityMarkers(t, script,
+	workflow := readHostedCapabilityFile(t, hostedCapabilityWorkflow)
+	requireHostedCapabilityMarkers(t, workflow,
 		"outer.forward.iptables_policy=accept",
 		"outer.forward.iptables_policy=drop",
 	)
