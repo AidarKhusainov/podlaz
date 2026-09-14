e86d5f8fd4c4c109e01befe000e5d0c3317b3f17
Aidar Khusainov <khusainov19978@gmail.com>
2026-09-14 16:39:45 +0500

    test: require scoped hosted forwarding ownership

---
+++ scripts/e2e/hosted_e2e_capability_forwarding_contract_test.go
@@ -0,0 +1,15 @@
+package e2e_test
+
+import "testing"
+
+func TestHostedE2ECapabilityOwnsScopedOuterForwarding(t *testing.T) {
+	script := readHostedCapabilityFile(t, hostedCapabilityScript)
+	requireHostedCapabilityMarkers(t, script,
+		"DOCKER-USER",
+		"podlaz-hosted-e2e-forward-out",
+		"podlaz-hosted-e2e-forward-in",
+		"-m conntrack --ctstate ESTABLISHED,RELATED",
+		"iptables -I",
+		"iptables -D",
+	)
+}
