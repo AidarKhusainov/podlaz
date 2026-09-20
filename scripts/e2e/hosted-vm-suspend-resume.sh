5c00ae668ec44f5a85c32b8f482e874aa9f7dae5
Aidar Khusainov <khusainov19978@gmail.com>
2026-09-20 09:23:14 +0500

    ci: satisfy hosted VM shell contract

--- scripts/e2e/hosted-vm-suspend-resume.sh
+++ scripts/e2e/hosted-vm-suspend-resume.sh
@@ -450,8 +450,10 @@ run_scenario() {
 
   mark_failure infrastructure vm.boot
   hosted_vm_start
-  hosted_vm_wait_ssh
+  hosted_vm_wait_ssh 180
   hosted_vm_wait_cloud_init
+  # Expansion is intentionally evaluated by the guest shell.
+  # shellcheck disable=SC2016
   hosted_vm_ssh '. /etc/os-release; test "$ID" = ubuntu; test "$VERSION_ID" = 24.04'
   record_evidence vm.boot pass
 
