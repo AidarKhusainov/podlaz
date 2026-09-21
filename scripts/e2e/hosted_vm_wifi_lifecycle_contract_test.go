9b21a73f42a7f1d8e863cebec13e105915512e9e
Aidar Khusainov <khusainov19978@gmail.com>
2026-09-21 22:21:25 +0500

    test: require routed Wi-Fi endpoint topology

--- scripts/e2e/hosted_vm_wifi_lifecycle_contract_test.go
+++ scripts/e2e/hosted_vm_wifi_lifecycle_contract_test.go
@@ -62,6 +62,8 @@ func TestHostedVMWiFiLifecycleKeepsControlOffTestedWiFi(t *testing.T) {
 		"iw phy \"${ap_phy}\" set netns",
 		"198.51.100.1/24",
 		"172.31.254.2/30",
+		"WIFI_ENDPOINT_IP=203.0.113.10",
+		"ip address add \"${endpoint_ip}/32\" dev lo",
 		"ip rule add priority \"${policy_priority}\"",
 		"ip route del default via \"${management_gateway}\"",
 		"ip -4 route show default | grep -F \"via 198.51.100.1 dev ${client_if}\"",
